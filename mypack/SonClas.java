fc37463d0ebe166fb266a8adbb20c59cc0d9e221
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-09-05 14:45:52 +0530

    concept of package

---
+++ mypack/SonClas.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class SonClass extends DellClass {
+    public void multiplication() {
+        int total;
+        total = a * b;
+        System.out.println("Multiplication=" + total);
+    }
+}
