3f5947167404812e6c2aa0dd79794b5fe982978a
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-09-07 00:37:39 +0530

    Represention of all data type

---
+++ Represention.java
@@ -0,0 +1,15 @@
+public class Represention
+{
+    public static void main(String args[])
+    {
+        int num=109;
+        byte b=13;
+        short a=45;
+        long num1=667890009999L;
+         float Fl=76.8f;
+         double l=657.7;
+         char ch='A';
+         String str="vikas";
+         System.out.print(num+str);
+    }
+}
