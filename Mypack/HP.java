6e42fbe495ebb3547726c8cb99130a01ec49a9aa
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-09-07 00:40:38 +0530

    concept of inheritance

---
+++ Mypack/HP.java
@@ -0,0 +1,12 @@
+package mypack;
+import mypack.DellClass;
+public class HP
+{
+            DellClass obj=new DellClass();
+            public void multiplication()
+            {
+                int total;
+                total=obj.a+obj.b;
+                System.out.println("Multiplication="+total);
+            }
+}
