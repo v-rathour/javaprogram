0963108cc77a9803a9c1d06ace3f5bd08c45decd
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-09-07 00:20:36 +0530

    Intialization of sting in java

---
+++ String Program/StringClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//public class StringClass
+//{
+//    public static void main(String args[])
+//    {
+//        String str=new String();
+//        Scanner obj=new Scanner(System.in);
+//        System.out.print("Enter the string:");
+//        str=obj.nextLine();
+//        System.out.println("String="+str);
+//    }
+//}
