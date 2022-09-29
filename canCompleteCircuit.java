d3162619d5fbdab59bef70dee4991614d67cdcac
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-09-29 10:41:08 +0530

    gas station leetcode134

---
+++ canCompleteCircuit.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int tc =0;
+        int curr=0;
+        int si=0;
+        for(int i=0;i<cost.length;i++){
+            tc=tc+gas[i]-cost[i];
+            curr +=gas[i]-cost[i];
+            if(curr<0){
+                curr=0;
+                si=i+1;
+            }
+        }
+        if(tc<0)
+            return -1;
+        return si;
+    } 
+    
+}
