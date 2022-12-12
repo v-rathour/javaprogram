32384a7dc812c6c69d1db59a92aebb34461f19c5
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-12-12 19:50:14 +0530

    Create Arrays_Spiral_Print_Clockwise.java

---
+++ Arrays_Spiral_Print_Clockwise.java
@@ -0,0 +1,36 @@
+public class Arrays_Spiral_Print_Clockwise {
+    public static void main(String[] args) {
+        int arr[][]={{11,12,13,14},{21,22,23,24},{31,32,33,34},{41,42,43,44}};
+         Print_Clockwise(arr);
+    }
+    public static void Print_Clockwise(int arr[][]){
+        int minc=0;
+        int minr=0;
+        int maxr=arr.length-1;
+        int maxc=arr[0].length-1;
+        int total= arr.length*arr[0].length;
+        int count=0;
+        while(count<total){
+            for (int i = minc; i <=maxc && count<total; i++) {
+                System.out.print(arr[minr][i]+", ");
+                count++;
+            }
+            minr++;
+            for (int i = minr; i <=maxr && count<total ; i++) {
+                System.out.print(arr[i][maxc]+", ");
+                count++;
+            }
+            maxc--;
+            for (int i = maxc; i >=minc && count<total ; i--) {
+                System.out.print(arr[maxr][i]+", ");
+                count++;
+            }
+            maxr--;
+            for (int i = maxr; i >=minr && count<total; i--) {
+                System.out.print(arr[i][minc]+", ");
+                count++;
+            }
+            minc++;
+        }
+    }
+}
