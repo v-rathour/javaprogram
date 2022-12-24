c07792bcf9d47b8b558185b09758511e9c3f3c31
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-12-24 06:35:14 +0530

    Create Union_of_two_Array.java

---
+++ Union_of_two_Array.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Union_of_two_Array {
+    public static void main(String[] args) {
+        int arr1[]={7,3,9};
+        int arr2[]={6,3,9,2,9,4};
+        HashSet<Integer> set=new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+        for(Integer value: set){
+            System.out.println(value);
+         }
+
+    }
+}
